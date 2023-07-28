58510f0ff8f751f618174abc839def5428c8f02e
radixx1 <phuddulaal@gmail.com>
2023-07-28 08:32:31 +0000

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from "./Components/Header";
+import { TweetForm } from "./Components/TweetForm";
+function App() {
+  return (
+    <>
+    <Header/>
+    <TweetForm/>
+    </>
+  );
+}
+
+export default App;
